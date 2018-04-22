Fix linearization sign bug

src/fnc/LMPC.py
@@ -1,7 +1,7 @@
-def LMPC(npG, L, npE, F, b, x0, np, qp, matrix, datetime, la, SS, Qfun, N, n, d, spmatrix, numSS_Points, Qslack, Q_LMPC, R_LMPC, it):
+def LMPC(npG, L, npE, F, b, x0, np, qp, matrix, datetime, la, SS, Qfun, N, n, d, spmatrix, numSS_Points, Qslack, Q_LMPC, R_LMPC, it, swifth):
 
-    SS_Points1, Sel_Qfun1 = SelectPoints(SS, Qfun, it-1, x0, numSS_Points/2, np, la)
-    SS_Points2, Sel_Qfun2 = SelectPoints(SS, Qfun, it-2, x0, numSS_Points/2, np, la)
+    SS_Points1, Sel_Qfun1 = SelectPoints(SS, Qfun, it-1, x0, numSS_Points/2, np, la, swifth)
+    SS_Points2, Sel_Qfun2 = SelectPoints(SS, Qfun, it-2, x0, numSS_Points/2, np, la, swifth)
 
     SS_Points = np.hstack((SS_Points1, SS_Points2))
 
@@ -29,7 +29,7 @@ def LMPC(npG, L, npE, F, b, x0, np, qp, matrix, datetime, la, SS, Qfun, N, n, d,
 
     return Sol, feasible, deltaTimer, slack
 
-def SelectPoints(SS, Qfun, it, x0, numSS_Points, np, la):
+def SelectPoints(SS, Qfun, it, x0, numSS_Points, np, la, swifth):
     x = SS[:, :, it]
 
     oneVec = np.ones((x.shape[0], 1))
@@ -38,8 +38,8 @@ def SelectPoints(SS, Qfun, it, x0, numSS_Points, np, la):
     norm = la.norm(diff, 1, axis=1)
     MinNorm = np.argmin(norm)
 
-    SS_Points = x[MinNorm:MinNorm + numSS_Points, :].T
-    Sel_Qfun = Qfun[MinNorm:MinNorm + numSS_Points, it]
+    SS_Points = x[swifth + MinNorm:swifth + MinNorm + numSS_Points, :].T
+    Sel_Qfun = Qfun[swifth + MinNorm:swifth + MinNorm + numSS_Points, it]
     return SS_Points, Sel_Qfun
 
 def ComputeCost(x, u, np, TrackLength):
@@ -175,9 +175,9 @@ def LMPC_BuildMatIneqConst(N, n, np, linalg, spmatrix, numSS_Points):
                    [ 0., 0., 0., 0., 0., 1.],
                    [ 0., 0., 0., 0., 0.,-1.]])
 
-    bx = np.array([[ 10.], # vx max
-                   [ 1.], # max ey
-                   [ 1.]])# max ey
+    bx = np.array([[ 3.], # vx max
+                   [ 0.8], # max ey
+                   [ 0.8]])# max ey
 
     # Buil the matrices for the input constraint in each region. In the region i we want Fx[i]x <= bx[b]
     Fu = np.array([[ 1., 0.],

src/fnc/SysID.py
@@ -1,13 +1,13 @@
-def LMPC_EstimateABC(LinPoints, N, n, d, SS, uSS, TimeSS, qp, matrix, PointAndTangent, dt, it):
+def LMPC_EstimateABC(LinPoints, LinInput, N, n, d, SS, uSS, TimeSS, qp, matrix, PointAndTangent, dt, it):
     import numpy as np
     from SysModel import Curvature
     import datetime
 
     Atv = []; Btv = []; Ctv = []; indexUsed_list = []
 
-    usedIt = range(it-1,it)
+    usedIt = range(it-2,it)
 
-    for i in range(0, N + 1):
+    for i in range(0, N):
         MaxNumPoint = 200 # Need to reason on how these points are selected
         x0 = LinPoints[i, :]
 
@@ -19,14 +19,25 @@ def LMPC_EstimateABC(LinPoints, N, n, d, SS, uSS, TimeSS, qp, matrix, PointAndTa
         h = 5
         lamb = 0.0
         stateFeatures = [0, 1, 2]
-        scaling = np.array([[1.0, 0.0, 0.0],
-                            [0.0, 1.0, 0.0],
-                            [0.0, 0.0, 1.0]])
+        ConsiderInput = 1
+
+        if ConsiderInput == 1:
+            scaling = np.array([[0.1, 0.0, 0.0, 0.0, 0.0],
+                                [0.0, 1.0, 0.0, 0.0, 0.0],
+                                [0.0, 0.0, 1.0, 0.0, 0.0],
+                                [0.0, 0.0, 0.0, 1.0, 0.0],
+                                [0.0, 0.0, 0.0, 0.0, 1.0]])
+            xLin = np.hstack((LinPoints[i, stateFeatures], LinInput[i, :]))
+        else:
+            scaling = np.array([[1.0, 0.0, 0.0],
+                                [0.0, 1.0, 0.0],
+                                [0.0, 0.0, 1.0]])
+            xLin = LinPoints[i, stateFeatures]
 
         indexSelected = []
         K = []
         for i in usedIt:
-            indexSelected_i, K_i = ComputeIndex(h, SS, TimeSS, i, x0, stateFeatures, scaling, MaxNumPoint)
+            indexSelected_i, K_i = ComputeIndex(h, SS, uSS, TimeSS, i, xLin, stateFeatures, scaling, MaxNumPoint, ConsiderInput)
             indexSelected.append(indexSelected_i)
             K.append(K_i)
 
@@ -72,7 +83,7 @@ def LMPC_EstimateABC(LinPoints, N, n, d, SS, uSS, TimeSS, qp, matrix, PointAndTa
         depsi_wz   =      dt
         depsi_epsi =  1 - dt * ( -vx * np.sin(epsi) - vy * np.cos(epsi) ) / den * cur
         depsi_s    =      0                                                                      # Because cur = constant
-        depsi_ey   =    - dt * (vx * np.cos(epsi) - vy * np.sin(epsi)) / (den**2) * cur * (-cur)
+        depsi_ey   =      dt * (vx * np.cos(epsi) - vy * np.sin(epsi)) / (den**2) * cur * (-cur)
 
         Ai[3, :]   = [depsi_vx, depsi_vy, depsi_wz, depsi_epsi, depsi_s, depsi_ey]
 
@@ -84,7 +95,7 @@ def LMPC_EstimateABC(LinPoints, N, n, d, SS, uSS, TimeSS, qp, matrix, PointAndTa
         ds_wz    =  0
         ds_epsi  =  dt * (-vx * np.sin(epsi) - vy * np.cos(epsi)) / den
         ds_s     = 1 #+ Ts * (Vx * cos(epsi) - Vy * sin(epsi)) / (1 - ey * rho) ^ 2 * (-ey * drho);
-        ds_ey    =  dt * ( vx * np.cos(epsi) - vy * np.sin(epsi)) / (( den )**2)* (-cur)
+        ds_ey    = -dt * ( vx * np.cos(epsi) - vy * np.sin(epsi)) / ( den*2)* (-cur)
 
         Ai[4, :] = [ds_vx, ds_vy, ds_wz, ds_epsi, ds_s, ds_ey]
 
@@ -172,7 +183,7 @@ def LMPC_LocLinReg(Q, b, stateFeatures, inputFeatures, qp):
 
     return A, B, C
 
-def ComputeIndex(h, SS, TimeSS, it, x0, stateFeatures, scaling, MaxNumPoint):
+def ComputeIndex(h, SS, uSS, TimeSS, it, x0, stateFeatures, scaling, MaxNumPoint, ConsiderInput):
     import numpy as np
     from numpy import linalg as la
     import datetime
@@ -183,8 +194,19 @@ def ComputeIndex(h, SS, TimeSS, it, x0, stateFeatures, scaling, MaxNumPoint):
 
     # What to learn a model such that: x_{k+1} = A x_k  + B u_k + C
     oneVec = np.ones( (SS[0:TimeSS[it], :, it].shape[0]-1, 1) )
-    x0Vec = (np.dot( np.array([x0[stateFeatures]]).T, oneVec.T )).T
-    diff  = np.dot(( SS[0:TimeSS[it], :, it][0:-1, stateFeatures] - x0Vec ), scaling)
+
+    x0Vec = (np.dot( np.array([x0]).T, oneVec.T )).T
+
+    if ConsiderInput == 1:
+        DataMatrix = np.hstack((SS[0:TimeSS[it]-1, stateFeatures, it], uSS[0:TimeSS[it]-1, :, it]))
+    else:
+        DataMatrix = SS[0:TimeSS[it]-1, stateFeatures, it]
+
+    # np.savetxt('A.csv', SS[0:TimeSS[it]-1, stateFeatures, it], delimiter=',', fmt='%f')
+    # np.savetxt('B.csv', SS[0:TimeSS[it], :, it][0:-1, stateFeatures], delimiter=',', fmt='%f')
+    # np.savetxt('SS.csv', SS[0:TimeSS[it], :, it], delimiter=',', fmt='%f')
+
+    diff  = np.dot(( DataMatrix - x0Vec ), scaling)
     # print 'x0Vec \n',x0Vec
     norm = la.norm(diff, 1, axis=1)
     indexTot =  np.squeeze(np.where(norm < h))

src/fnc/SysModel.py
@@ -82,9 +82,9 @@ def DynModel(x, x_glob, u, np, dt, PointAndTangent):
 
 
         # Noises
-        noise_vx = np.maximum(-0.05, np.min(np.random.randn()*0.001, 0.05))
-        noise_vy = np.maximum(-0.01, np.min(np.random.randn()*0.001, 0.01))
-        noise_wz = np.maximum(-0.01, np.min(np.random.randn()*0.001, 0.01))
+        noise_vx = np.maximum(-0.05, np.min(np.random.randn()*0.01, 0.05))
+        noise_vy = np.maximum(-0.1, np.min(np.random.randn()*0.01, 0.1))
+        noise_wz = np.maximum(-0.1, np.min(np.random.randn()*0.01, 0.1))
 
         # Update the value of the states
         psi  = x_next[3]

src/fnc/Track.py
@@ -24,36 +24,48 @@ def Evaluate_e_ey(s, ey, PointAndTangent):
         r = 1 / PointAndTangent[i, 5]           # Extract curvature
         ang = PointAndTangent[i-1, 2]           # Extract angle of the tangent at the initial point (i-1)
         # Compute the center of the arc
-        CenterX = PointAndTangent[i-1, 0] + r * np.cos(ang + np.pi / 2)
-        CenterY = PointAndTangent[i-1, 1] + r * np.sin(ang + np.pi / 2)
-        # Compute the spanned angle
-        spanAng = (s - PointAndTangent[i, 3] ) / (np.pi * np.abs(r)) * np.pi
+        if r >= 0:
+            direction = 1
+        else:
+            direction = -1
 
-        # Finally compute the point
-        x = CenterX + (np.abs(r) + ey) * np.cos(np.pi / 2 - spanAng + ang)
-        y = CenterY + (np.abs(r) + ey) * np.sin(np.pi / 2 - spanAng + ang)
+        CenterX = PointAndTangent[i-1, 0] \
+                  + np.abs(r) * np.cos(ang + direction * np.pi / 2)  # x coordinate center of circle
+        CenterY = PointAndTangent[i-1, 1] \
+                  + np.abs(r) * np.sin(ang + direction * np.pi / 2)  # y coordinate center of circle
+
+        spanAng = (s - PointAndTangent[i, 3]) / (np.pi * np.abs(r)) * np.pi
+
+        angleNormal = wrap((direction * np.pi / 2 + ang))
+        angle = -(np.pi - np.abs(angleNormal)) * (sign(angleNormal))
+
+        x = CenterX + (np.abs(r) - direction * ey) * np.cos(angle + direction * spanAng)  # x coordinate of the last point of the segment
+        y = CenterY + (np.abs(r) - direction * ey) * np.sin(angle + direction * spanAng)  # y coordinate of the last point of the segment
 
     return x, y
 
 def CreateTrack():
     import numpy as np
+    import matplotlib.pyplot as plt
     # The track is created stucking lines and arc of circles. In particular, each line of spec specifies a segment of
     # length s of radius of curvature r. Note that the radius of curvature is negative for counter clock-wise curve.
-    spec = np.array([[5            ,  0],
-                     [2 * np.pi / 2, -2], # Note s = 1 * np.pi / 2 and r = -1 ---> Angle spanned = np.pi / 2
-                     [5           ,   0],
-                     [2 * np.pi / 2, -2],
-                     [10           ,  0],
-                     [2 * np.pi / 2, -2],
-                     [5           ,   0],
-                     [2 * np.pi / 2, -2]])
+    spec = np.array([[60 * 0.03,  0],
+                     [80 * 0.03, -80 * 0.03 * 2 / np.pi], # Note s = 1 * np.pi / 2 and r = -1 ---> Angle spanned = np.pi / 2
+                     [20 * 0.03,   0],
+                     [80 * 0.03, -80 * 0.03 * 2 / np.pi],
+                     [40 * 0.03, +40 * 0.03 * 10/ np.pi],
+                     [60 * 0.03, -60 * 0.03 * 5 / np.pi],
+                     [40 * 0.03, +40 * 0.03 * 10/ np.pi],
+                     [80 * 0.03, -80 * 0.03 * 2 / np.pi],
+                     [20 * 0.03, 0],
+                     [80 * 0.03, -80 * 0.03 * 2 / np.pi]])
 
     # Now given the above segments we compute the (x, y) points of the track and the angle of the tangent vector (psi) at
     # these points. For each segment we compute the (x, y, psi) coordinate at the last point of the segment. Furthermore,
     # we compute also the cumulative s at the starting point of the segment at signed curvature
     # PointAndTangent = [x, y, psi, cumulative s, segment length, signed curvature]
     PointAndTangent = np.zeros((spec.shape[0] + 2, 6))
-
+    # plt.figure(1000)
     for i in range(0, spec.shape[0]):
         if spec[i, 1] == 0.0:                              # If the current segment is a straight line
             l   = spec[i, 0]                               # Length of the segments
@@ -73,23 +85,35 @@ def CreateTrack():
             l   = spec[i, 0]                              # Length of the segment
             r   = spec[i, 1]                              # Radius of curvature
             ang = PointAndTangent[i, 2]                   # Angle of the tangent vector at the starting point of the segment
-            CenterX = PointAndTangent[i, 0] \
-                      + r * np.cos(ang + np.pi / 2)       # x coordinate center of circle
-            CenterY = PointAndTangent[i, 1] \
-                      + r * np.sin(ang + np.pi / 2)       # y coordinate center of circle
-            spanAng = l / np.abs(r)                       # Angle spanned by the circle
-            psi     = ang + spanAng * np.sign(r)          # Angle of the tangent vector at the last point of the segment
 
-            x   = CenterX + np.abs(r)*np.cos(np.pi/2 - spanAng + ang) # x coordinate of the last point of the segment
-            y   = CenterY + np.abs(r)*np.sin(np.pi/2 - spanAng + ang) # y coordinate of the last point of the segment
+            if r >= 0:
+                direction = 1
+            else:
+                direction = -1
+
+            CenterX = PointAndTangent[i, 0] \
+                      + np.abs(r) * np.cos(ang + direction * np.pi / 2)       # x coordinate center of circle
+            CenterY = PointAndTangent[i, 1] \
+                      + np.abs(r) * np.sin(ang + direction * np.pi / 2)       # y coordinate center of circle
+            spanAng = l / np.abs(r)                       # Angle spanned by the circle
+            psi     = wrap( ang + spanAng * np.sign(r) )       # Angle of the tangent vector at the last point of the segment
+
+            angleNormal = wrap( (direction * np.pi/2 + ang) )
+            angle = -( np.pi - np.abs(angleNormal) ) * (sign(angleNormal))
+            x   = CenterX + np.abs(r)*np.cos(angle + direction*spanAng) # x coordinate of the last point of the segment
+            y   = CenterY + np.abs(r)*np.sin(angle + direction*spanAng) # y coordinate of the last point of the segment
 
             # With the above information create the new line
+            # plt.plot(CenterX, CenterY, 'bo')
+            # plt.plot(x, y, 'ro')
+
             if i == 0:
                 NewLine = np.array([ x, y, psi, PointAndTangent[i, 3], l, 1/r])
             else:
                 NewLine = np.array([x, y, psi, PointAndTangent[i, 3] + PointAndTangent[i, 4], l, 1/r])
 
             PointAndTangent[i+1, :] = NewLine # Write the new info
+        # plt.plot(x, y, 'or')
 
     # Now update info on last point
     xs   = PointAndTangent[PointAndTangent.shape[0] - 2, 0]
@@ -98,6 +122,8 @@ def CreateTrack():
     yf   = PointAndTangent[0, 1]
     psif = PointAndTangent[PointAndTangent.shape[0] - 2, 2]
 
+    # plt.plot(xf, yf, 'or')
+    # plt.show()
     l  = np.sqrt( (xf - xs)**2 + (yf - ys)**2)
 
     NewLine = np.array([xf, yf, psif, PointAndTangent[PointAndTangent.shape[0] - 2, 3] + PointAndTangent[PointAndTangent.shape[0] - 2, 4],  l, 0])
@@ -107,3 +133,22 @@ def CreateTrack():
     np.set_printoptions(precision=2)
     print "Track length:", PointAndTangent[-1,3]+PointAndTangent[-1,4]
     return PointAndTangent, PointAndTangent[-1,3]+PointAndTangent[-1,4]
+
+def wrap(angle):
+    import numpy as np
+    if angle < -np.pi:
+        w_angle = 2 * np.pi + angle
+    elif angle > np.pi:
+        w_angle = angle - 2 * np.pi
+    else:
+        w_angle = angle
+
+    return w_angle
+
+def sign(a):
+    if a >= 0:
+        res = 1
+    else:
+        res = -1
+
+    return res

src/main.py
@@ -29,15 +29,15 @@ RunLMPC    = 1; plotFlagLMPC   = 1
 # ======================================================================================================================
 # ====================================== INITIALIZE PARAMETERS =========================================================
 # ======================================================================================================================
-dt = 1.0/50.0                      # Controller discretization time
+dt = 1.0/10.0                      # Controller discretization time
 Time = 100                          # Simulation time
 Points = int(Time/dt)              # Number of points in the simulation
 u = np.zeros((Points, 2))          # Initialize the input vector
 x      = np.zeros((Points+1, 6))   # Initialize state vector (In curvilinear abscissas)
 x_glob = np.zeros((Points+1, 6))   # Initialize the state vector in absolute reference frame
-vt = 2
+vt = 0.8
 
-x[0,0] = 1.5; x_glob[0,0] = 1.5
+x[0,0] = 0.5; x_glob[0,0] = 0.5
 x[0,4] = 0.5; x_glob[0,4] = 0.5
 # Create the track. The vector PointAndTangent = [xf yf]
 PointAndTangent, TrackLength = CreateTrack()
@@ -47,7 +47,7 @@ PointAndTangent, TrackLength = CreateTrack()
 if RunPID == 1:
     for i in range(0, Points):
         u[i, 0] = - 0.6 * x[i, 5] - 0.9 * x[i, 3] + np.maximum(-0.9, np.min(np.random.randn()*0.25, 0.9))
-        u[i, 1] = vt - x[i, 0] + np.maximum(-0.5, np.min(np.random.randn()*0.15, 0.5))
+        u[i, 1] = 1.5*(vt - x[i, 0]) + np.maximum(-0.2, np.min(np.random.randn()*0.10, 0.2))
         x[i+1, :], x_glob[i+1, :] = DynModel(x[i, :], x_glob[i, :], u[i, :], np, dt, PointAndTangent)
 
     print "Number of laps completed: ", int(np.floor(x[-1, 4] / (PointAndTangent[-1, 3] + PointAndTangent[-1, 4])))
@@ -67,10 +67,10 @@ print "A matrix: \n", A, "\n B matrix: \n", B
 
 n = 6
 d = 2
-N = 8
+N = 12
 
-Q = np.diag([10.0, 0.0, 0, 1, 0.0, 100.0]) # vx, vy, wz, epsi, s, ey
-R = np.diag([10.0, 1.0]) # delta, a
+Q = np.diag([1.0, 1.0, 1, 1, 0.0, 100.0]) # vx, vy, wz, epsi, s, ey
+R = np.diag([1.0, 10.0]) # delta, a
 
 M, q    = BuildMatCost(Q, R, Q, N, linalg, np, spmatrix, vt)
 F, b    = BuildMatIneqConst(N, n, np, linalg, spmatrix)
@@ -184,7 +184,7 @@ PointsLMPC = int(TimeLMPC / dt)            # Number of points in the simulation
 uLMPC = np.zeros((PointsLMPC, 2))          # Initialize the input vector
 xLMPC      = np.zeros((PointsLMPC+1, 6))   # Initialize state vector (In curvilinear abscissas)
 x_globLMPC = np.zeros((PointsLMPC+1, 6))   # Initialize the state vector in absolute reference frame
-Laps       = 20
+Laps       = 5
 
 xLMPC[0,:] = x[0,:]
 x_globLMPC[0,:] = x_glob[0,:]
@@ -192,6 +192,7 @@ x_globLMPC[0,:] = x_glob[0,:]
 LinPoints = xMPC_tv[0:N+1,:]
 
 numSS_Points = 100
+swifth = N-1
 
 TimeSS= 10000*np.ones(Laps+2)
 SS    = 10000*np.ones((2*xMPC_tv.shape[0], 6, Laps+2))
@@ -199,10 +200,10 @@ uSS   = 10000*np.ones((2*xMPC_tv.shape[0], 2, Laps+2))
 Qfun  = 0*np.ones((2*xMPC_tv.shape[0], Laps+2)) # Need to initialize at zero as adding point on the fly
 
 # Adding Trajectory to safe set iteration 0
-TimeSS[0] = xMPC.shape[0]
-SS[0:TimeSS[0],:, 0]  = xMPC
-uSS[0:TimeSS[0]-1,:, 0]  = uMPC
-Qfun[0:TimeSS[0], 0] = ComputeCost(xMPC, uMPC, np, TrackLength)
+TimeSS[0] = x.shape[0]
+SS[0:TimeSS[0],:, 0]  = x
+uSS[0:TimeSS[0]-1,:, 0]  = u
+Qfun[0:TimeSS[0], 0] = ComputeCost(x, u, np, TrackLength)
 
 # Adding Trajectory to safe set iteration 1
 TimeSS[1] = xMPC_tv.shape[0]
@@ -214,9 +215,9 @@ print Qfun[0:TimeSS[0], 0], Qfun[0:TimeSS[1], 1]
 
 F_LMPC, b_LMPC = LMPC_BuildMatIneqConst(N, n, np, linalg, spmatrix, numSS_Points)
 
-Qslack = 1*np.diag([100, 1, 1, 1, 100, 500])
-Q_LMPC = 0 * np.diag([0.0, 1.0, 10.0, 0.0, 0.0, 0.0])  # vx, vy, wz, epsi, s, ey
-R_LMPC = 1 * np.diag([1.0, 0.1])  # delta, a
+Qslack = 50*np.diag([1, 10, 10, 10, 10, 10])
+Q_LMPC = 0 * np.diag([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])  # vx, vy, wz, epsi, s, ey
+R_LMPC = 5 * np.diag([1.0, 1.0])  # delta, a
 
 if PlotIndex == 1:
     plt.figure(105)
@@ -310,13 +311,15 @@ if RunLMPC == 1:
             else:
                 startTimer = datetime.datetime.now()  # Start timer for LMPC iteration
 
-                Atv, Btv, Ctv, indexUsed_list = LMPC_EstimateABC(LinPoints, N, n, d, SS, uSS, TimeSS, qp, matrix,
+                Atv, Btv, Ctv, indexUsed_list = LMPC_EstimateABC(LinPoints, LinInput, N, n, d, SS, uSS, TimeSS, qp, matrix,
                                                                  PointAndTangent, dt, it)
                 endTimer = datetime.datetime.now(); deltaTimer_tv = endTimer - startTimer
                 # Atv0, Btv0, Ctv0 = EstimateABC(LinPoints, N, n, d, x_ID, u_ID, qp, matrix, PointAndTangent, dt)
                 # print (Atv0[0]==Atv[0]).all()
-
+                #
                 G, E, L, npG, npE = LMPC_BuildMatEqConst(Atv, Btv, Ctv, N, n, d, np, spmatrix, 1)
+                # G, E, L, npG, npE = LMPC_BuildMatEqConst(A, B, np.zeros((n, 1)), N, n, d, np, spmatrix, 0)
+
                 if PlotIndex == 1:
                     line.set_xdata(x_globMPC_tv[indexUsed_list[0], 4])
                     line.set_ydata(x_globMPC_tv[indexUsed_list[0], 5])
@@ -327,10 +330,14 @@ if RunLMPC == 1:
 
 
 
-            Sol, feasible, deltaTimer, slack = LMPC(npG, L, npE, F_LMPC, b_LMPC, x0, np, qp, matrix, datetime, la, SS, Qfun,  N, n, d, spmatrix, numSS_Points, Qslack, Q_LMPC, R_LMPC, it)
+            Sol, feasible, deltaTimer, slack = LMPC(npG, L, npE, F_LMPC, b_LMPC, x0, np, qp, matrix, datetime, la, SS,
+                                                    Qfun,  N, n, d, spmatrix, numSS_Points, Qslack, Q_LMPC, R_LMPC, it, swifth)
 
             xPred, uPred = GetPred(Sol, n, d, N, np)
+
             LinPoints = np.vstack((xPred.T[1:,:], xPred.T[-1,:]))
+            LinInput = uPred.T
+
             uLMPC[i, :] = uPred[:, 0]
             xLMPC[i + 1, :], x_globLMPC[absTime + 1, :] = DynModel(xLMPC[i, :], x_globLMPC[absTime, :], uLMPC[i, :], np, dt,
                                                              PointAndTangent)
@@ -352,8 +359,6 @@ if RunLMPC == 1:
                 plt.draw()
                 plt.pause(1e-17)
 
-
-
             if i <= 5:
                 print("Linearization time: %.4fs Solver time: %.4fs" % (deltaTimer_tv.total_seconds(), deltaTimer.total_seconds()))
                 print "Time: ", i * dt, "Current State and Input: ", xLMPC[i, :], uLMPC[i, :]
@@ -370,6 +375,7 @@ if RunLMPC == 1:
             absTime = absTime + 1
 
 
+
     np.savez('LMPC_PathFollowing', x=xLMPC, u=uLMPC, x_glob=x_globLMPC)
 else:
     data = np.load('LMPC_PathFollowing.npz')
@@ -377,11 +383,12 @@ else:
     uLMPC      = data['u']
     x_globLMPC = data['x_glob']
 
-it = 2 + Laps
-SS[0:i, :, it-1]  = xLMPC[0:i, :]
-uSS[0:i-1, :, it-1] = uLMPC[0:i-1, :]
-TimeSS[it-1] = i
-Qfun[0:i, it-1] = ComputeCost(xLMPC[0:i, :], xLMPC[0:i-1, :], np, TrackLength)
+if RunLMPC == 1:
+    it = 2 + Laps
+    SS[0:i, :, it-1]  = xLMPC[0:i, :]
+    uSS[0:i-1, :, it-1] = uLMPC[0:i-1, :]
+    TimeSS[it-1] = i
+    Qfun[0:i, it-1] = ComputeCost(xLMPC[0:i, :], xLMPC[0:i-1, :], np, TrackLength)
 
 print "===== LMPC terminated"
 # ======================================================================================================================
@@ -390,14 +397,15 @@ print "===== LMPC terminated"
 for i in range(0,Laps+2):
     print "Cost at iteration ", i, " is ",Qfun[0,i]
 print "===== Start Plotting"
+width = 0.8
 if plotFlag == 1:
     Points = np.floor(10*(PointAndTangent[-1, 3] + PointAndTangent[-1, 4]))
     Points1 = np.zeros((Points,2))
     Points2 = np.zeros((Points,2))
     Points0 = np.zeros((Points,2))
     for i in range(0,int(Points)):
-        Points1[i, :] = Evaluate_e_ey(i*0.1, 1.5, PointAndTangent)
-        Points2[i, :] = Evaluate_e_ey(i*0.1,-1.5, PointAndTangent)
+        Points1[i, :] = Evaluate_e_ey(i*0.1, width, PointAndTangent)
+        Points2[i, :] = Evaluate_e_ey(i*0.1,-width, PointAndTangent)
         Points0[i, :] = Evaluate_e_ey(i*0.1, 0, PointAndTangent)
 
     plt.figure(1)
@@ -436,8 +444,8 @@ if plotFlagMPC == 1:
     Points2 = np.zeros((Points,2))
     Points0 = np.zeros((Points,2))
     for i in range(0,int(Points)):
-        Points1[i, :] = Evaluate_e_ey(i*0.1, 1.5, PointAndTangent)
-        Points2[i, :] = Evaluate_e_ey(i*0.1,-1.5, PointAndTangent)
+        Points1[i, :] = Evaluate_e_ey(i*0.1, width, PointAndTangent)
+        Points2[i, :] = Evaluate_e_ey(i*0.1,-width, PointAndTangent)
         Points0[i, :] = Evaluate_e_ey(i*0.1, 0, PointAndTangent)
 
     plt.figure(3)
@@ -477,8 +485,8 @@ if plotFlagMPC_tv == 1:
     Points2 = np.zeros((Points,2))
     Points0 = np.zeros((Points,2))
     for i in range(0,int(Points)):
-        Points1[i, :] = Evaluate_e_ey(i*0.1, 1.5, PointAndTangent)
-        Points2[i, :] = Evaluate_e_ey(i*0.1,-1.5, PointAndTangent)
+        Points1[i, :] = Evaluate_e_ey(i*0.1, width, PointAndTangent)
+        Points2[i, :] = Evaluate_e_ey(i*0.1,-width, PointAndTangent)
         Points0[i, :] = Evaluate_e_ey(i*0.1, 0, PointAndTangent)
 
     plt.figure(5)
@@ -517,8 +525,8 @@ if plotFlagLMPC == 1:
     Points2 = np.zeros((Points,2))
     Points0 = np.zeros((Points,2))
     for i in range(0,int(Points)):
-        Points1[i, :] = Evaluate_e_ey(i*0.1, 1.5, PointAndTangent)
-        Points2[i, :] = Evaluate_e_ey(i*0.1,-1.5, PointAndTangent)
+        Points1[i, :] = Evaluate_e_ey(i*0.1, width, PointAndTangent)
+        Points2[i, :] = Evaluate_e_ey(i*0.1,-width, PointAndTangent)
         Points0[i, :] = Evaluate_e_ey(i*0.1, 0, PointAndTangent)
 
     plt.figure(7)