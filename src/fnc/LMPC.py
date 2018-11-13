Fix bugs in zVector computation

src/fnc/LMPC.py
@@ -6,8 +6,6 @@ from scipy import linalg
 from scipy import sparse
 from cvxopt.solvers import qp
 import datetime
-from functools import partial
-from pathos.multiprocessing import ProcessingPool as Pool
 from Utilities import Curvature
 from numpy import hstack, inf, ones
 from scipy.sparse import vstack
@@ -24,7 +22,7 @@ class ControllerLMPC():
         update: this function can be used to set SS, Qfun, uSS and the iteration index.
     """
 
-    def __init__(self, numSS_Points, numSS_it, N, Qslack, Qlane, Q, R, dR, n, d, shift, dt,  map, Laps, TimeLMPC, Solver):
+    def __init__(self, numSS_Points, numSS_it, N, Qslack, Qlane, Q, R, dR, dt,  map, Laps, TimeLMPC, Solver, inputConstraints):
         """Initialization
         Arguments:
             numSS_Points: number of points selected from the previous trajectories to build SS
@@ -47,30 +45,31 @@ class ControllerLMPC():
         self.Q = Q
         self.R = R
         self.dR = dR
-        self.n = n
-        self.d = d
-        self.shift = shift
+        self.n = Q.shape[1]
+        self.d = R.shape[1]
         self.dt = dt
         self.map = map
         self.Solver = Solver
         self.LapTime = 0
-        self.itUsedSysID = 2
+        self.itUsedSysID = 1
+        self.inputConstraints = inputConstraints
 
         self.OldInput = np.zeros((1,2))
 
         # Initialize the following quantities to avoid dynamic allocation
         NumPoints = int(TimeLMPC / dt) + 1
-        self.TimeSS  = 10000 * np.ones(Laps).astype(int)        # Time at which each j-th iteration is completed
-        self.SS      = 10000 * np.ones((NumPoints, 6, Laps))    # Sampled Safe SS
-        self.uSS     = 10000 * np.ones((NumPoints, 2, Laps))    # Input associated with the points in SS
-        self.Qfun    =     0 * np.ones((NumPoints, Laps))       # Qfun: cost-to-go from each point in SS
-        self.SS_glob = 10000 * np.ones((NumPoints, 6, Laps))    # SS in global (X-Y) used for plotting
+        self.LapCounter = 10000 * np.ones(Laps).astype(int)        # Time at which each j-th iteration is completed
+        self.TimeSS     = 10000 * np.ones(Laps).astype(int)        # Time at which each j-th iteration is completed
+        self.SS         = 10000 * np.ones((NumPoints, 6, Laps))    # Sampled Safe SS
+        self.uSS        = 10000 * np.ones((NumPoints, 2, Laps))    # Input associated with the points in SS
+        self.Qfun       =     0 * np.ones((NumPoints, Laps))       # Qfun: cost-to-go from each point in SS
+        self.SS_glob    = 10000 * np.ones((NumPoints, 6, Laps))    # SS in global (X-Y) used for plotting
+
+        self.zVector = np.array([0.0, 0.0, 0.0, 0.0, 10.0, 0.0])
+
 
         # Initialize the controller iteration
         self.it      = 0
-
-        # Initialize pool for parallel computing used in the internal function _LMPC_EstimateABC
-        self.p = Pool(4)
 
         # Build matrices for inequality constraints
         self.F, self.b = _LMPC_BuildMatIneqConst(self)
@@ -91,24 +90,30 @@ class ControllerLMPC():
         N = self.N; dt = self.dt
         it           = self.it
         numSS_Points = self.numSS_Points
-        shift       = self.shift
         Qslack       = self.Qslack
-        p = self.p
 
         LinPoints = self.LinPoints
         LinInput  = self.LinInput
         map = self.map
 
         # Select Points from SS
+        if (self.zVector[4]-x0[4] > map.TrackLength/2):
+            self.zVector[4] = np.max([self.zVector[4] - map.TrackLength,0])
+            self.LinPoints[4,-1] = self.LinPoints[4,-1]- map.TrackLength
+
+
         sortedLapTime = np.argsort(self.Qfun[0, 0:it])
 
         SS_PointSelectedTot = np.empty((n, 0))
-        SS_glob_PointSelectedTot = np.empty((n, 0))
+        Succ_SS_PointSelectedTot = np.empty((n, 0))
+        Succ_uSS_PointSelectedTot = np.empty((d, 0))
         Qfun_SelectedTot = np.empty((0))
         for jj in sortedLapTime[0:self.numSS_it]:
-            SS_PointSelected, Qfun_Selected = _SelectPoints(self, jj, x0, numSS_Points / self.numSS_it, shift)
-            SS_PointSelectedTot = np.append(SS_PointSelectedTot, SS_PointSelected, axis=1)
-            Qfun_SelectedTot = np.append(Qfun_SelectedTot, Qfun_Selected, axis=0)
+            SS_PointSelected, uSS_PointSelected, Qfun_Selected = _SelectPoints(self, jj, self.zVector, numSS_Points / self.numSS_it + 1)
+            Succ_SS_PointSelectedTot =  np.append(Succ_SS_PointSelectedTot, SS_PointSelected[:,1:], axis=1)
+            Succ_uSS_PointSelectedTot =  np.append(Succ_uSS_PointSelectedTot, uSS_PointSelected[:,1:], axis=1)
+            SS_PointSelectedTot      = np.append(SS_PointSelectedTot, SS_PointSelected[:,0:-1], axis=1)
+            Qfun_SelectedTot         = np.append(Qfun_SelectedTot, Qfun_Selected[0:-1], axis=0)
 
         self.SS_PointSelectedTot = SS_PointSelectedTot
         self.Qfun_SelectedTot = Qfun_SelectedTot
@@ -136,7 +141,7 @@ class ControllerLMPC():
             Solution = np.squeeze(res_cons['x'])     
 
         elif self.Solver == "OSQP":
-            # Adaptarion for QSQP from https://github.com/alexbuyval/RacingLMPC/
+            # Adaptation for QSQP from https://github.com/alexbuyval/RacingLMPC/
             res_cons, feasible = osqp_solve_qp(sparse.csr_matrix(M), q, sparse.csr_matrix(F), b, sparse.csr_matrix(G), np.add(np.dot(E,x0),L[:,0]))
             Solution = res_cons.x
 
@@ -148,15 +153,18 @@ class ControllerLMPC():
         # Extract solution and set linerizations points
         xPred, uPred, lambd, slack = _LMPC_GetPred(Solution, n, d, N, np)
 
+        self.zVector = np.dot(Succ_SS_PointSelectedTot, lambd)
+        self.uVector = np.dot(Succ_uSS_PointSelectedTot, lambd)
+
         self.xPred = xPred.T
         if self.N == 1:
             self.uPred    = np.array([[uPred[0], uPred[1]]])
             self.LinInput =  np.array([[uPred[0], uPred[1]]])
         else:
             self.uPred = uPred.T
-            self.LinInput = np.vstack((uPred.T[1:, :], uPred.T[-1, :]))
+            self.LinInput = np.vstack((uPred.T[1:, :], self.uVector))
 
-        self.LinPoints = np.vstack((xPred.T[1:,:], xPred.T[-1,:]))
+        self.LinPoints = np.vstack((xPred.T[1:,:], self.zVector))
 
         self.OldInput = uPred.T[0,:]
 
@@ -168,6 +176,7 @@ class ControllerLMPC():
         it = self.it
 
         self.TimeSS[it] = ClosedLoopData.SimTime
+        self.LapCounter[it] = ClosedLoopData.SimTime
         self.SS[0:(self.TimeSS[it] + 1), :, it] = ClosedLoopData.x[0:(self.TimeSS[it] + 1), :]
         self.SS_glob[0:(self.TimeSS[it] + 1), :, it] = ClosedLoopData.x_glob[0:(self.TimeSS[it] + 1), :]
         self.uSS[0:self.TimeSS[it], :, it]      = ClosedLoopData.u[0:(self.TimeSS[it]), :]
@@ -183,7 +192,7 @@ class ControllerLMPC():
 
         self.it = self.it + 1
 
-    def addPoint(self, x, u, i):
+    def addPoint(self, x, u):
         """at iteration j add the current point to SS, uSS and Qfun of the previous iteration
         Arguments:
             x: current state
@@ -191,10 +200,15 @@ class ControllerLMPC():
             i: at the j-th iteration i is the time at which (x,u) are recorded
         """
         Counter = self.TimeSS[self.it - 1]
-        self.SS[Counter + i + 1, :, self.it - 1] = x + np.array([0, 0, 0, 0, self.map.TrackLength, 0])
-        self.uSS[Counter + i + 1, :, self.it - 1] = u
-        if self.Qfun[Counter + i + 1, self.it - 1] == 0:
-            self.Qfun[Counter + i + 1, self.it - 1] = self.Qfun[Counter + i, self.it - 1] - 1
+        self.SS[Counter, :, self.it - 1] = x + np.array([0, 0, 0, 0, self.map.TrackLength, 0])
+        self.uSS[Counter, :, self.it - 1] = u
+
+        # The above two lines are needed as the once the predicted trajectory has crossed the finish line the goal is
+        # to reach the end of the lap which is about to start
+        if self.Qfun[Counter, self.it - 1] == 0:
+            self.Qfun[Counter, self.it - 1] = self.Qfun[Counter, self.it - 1] - 1
+
+        self.TimeSS[self.it - 1] = self.TimeSS[self.it - 1] + 1
 
     def update(self, SS, uSS, Qfun, TimeSS, it, LinPoints, LinInput):
         """update controller parameters. This function is useful to transfer information among LMPC controller
@@ -336,8 +350,8 @@ def _LMPC_BuildMatIneqConst(LMPC):
     Fx = np.array([[0., 0., 0., 0., 0., 1.],
                    [0., 0., 0., 0., 0., -1.]])
 
-    bx = np.array([[0.4],  # max ey
-                   [0.4]])  # max ey
+    bx = np.array([[LMPC.map.halfWidth],  # max ey
+                   [LMPC.map.halfWidth]])  # max ey
 
     # Buil the matrices for the input constraint in each region. In the region i we want Fx[i]x <= bx[b]
     Fu = np.array([[1., 0.],
@@ -345,10 +359,10 @@ def _LMPC_BuildMatIneqConst(LMPC):
                    [0., 1.],
                    [0., -1.]])
 
-    bu = np.array([[0.5],  # Max Steering
-                   [0.5],  # Max Steering
-                   [1.],  # Max Acceleration
-                   [1.]])  # Max Acceleration
+    bu = np.array([[LMPC.inputConstraints[0,0]],  # Max Steering
+                   [LMPC.inputConstraints[0,1]],  # Min Steering
+                   [LMPC.inputConstraints[1,0]],   # Max Acceleration
+                   [LMPC.inputConstraints[1,1]]])  # Min Acceleration
 
 
 
@@ -419,8 +433,10 @@ def _LMPC_BuildMatIneqConst(LMPC):
     return F_return, b
 
 
-def _SelectPoints(LMPC, it, x0, numSS_Points, shift):
-    SS = LMPC.SS
+def _SelectPoints(LMPC, it, x0, numSS_Points):
+    SS  = LMPC.SS
+    uSS = LMPC.uSS
+    TimeSS = LMPC.TimeSS
     SS_glob = LMPC.SS_glob
     Qfun = LMPC.Qfun
     xPred = LMPC.xPred
@@ -428,7 +444,8 @@ def _SelectPoints(LMPC, it, x0, numSS_Points, shift):
     TrackLength = map.TrackLength
     currIt = LMPC.it
 
-    x = SS[:, :, it]
+    x = SS[:, 0:(TimeSS[it]-1), it]
+    u = uSS[:, 0:(TimeSS[it]-1), it]
     x_glob = SS_glob[:, :, it]
     oneVec = np.ones((x.shape[0], 1))
     x0Vec = (np.dot(np.array([x0]).T, oneVec.T)).T
@@ -436,8 +453,8 @@ def _SelectPoints(LMPC, it, x0, numSS_Points, shift):
     norm = la.norm(diff, 1, axis=1)
     MinNorm = np.argmin(norm)
 
-    if (MinNorm + shift >= 0):
-        indexSSandQfun = range(shift + MinNorm, shift + MinNorm + numSS_Points)
+    if (MinNorm - numSS_Points/2 >= 0):
+        indexSSandQfun = range(-numSS_Points/2 + MinNorm, numSS_Points/2 + MinNorm)
         # SS_Points = x[shift + MinNorm:shift + MinNorm + numSS_Points, :].T
         # SS_glob_Points = x_glob[shift + MinNorm:shift + MinNorm + numSS_Points, :].T
         # Sel_Qfun = Qfun[shift + MinNorm:shift + MinNorm + numSS_Points, it]
@@ -447,33 +464,25 @@ def _SelectPoints(LMPC, it, x0, numSS_Points, shift):
         # SS_glob_Points = x_glob[MinNorm:MinNorm + numSS_Points, :].T
         # Sel_Qfun = Qfun[MinNorm:MinNorm + numSS_Points, it]
 
-    SS_Points = x[indexSSandQfun, :].T
+    SS_Points  = x[indexSSandQfun, :].T
+    SSu_Points = u[indexSSandQfun, :].T
     SS_glob_Points = x_glob[indexSSandQfun, :].T
     Sel_Qfun = Qfun[indexSSandQfun, it]
 
-    # if xPred != []:
-    #     sPred = xPred[:, 4]
-    #     print sPred
-    #     print sPred > TrackLength, sum(sPred > TrackLength)
-    #     print (np.all((xPred[:, 4] > TrackLength) == False))
-
+    # Modify the cost if the predicion has crossed the finisch line
     if xPred == []:
-        # print "Here "
         Sel_Qfun = Qfun[indexSSandQfun, it]
     elif (np.all((xPred[:, 4] > TrackLength) == False)):
-        # print "Here 1"
         Sel_Qfun = Qfun[indexSSandQfun, it]
     elif it < currIt - 1:
-        # print "Here 2"
         Sel_Qfun = Qfun[indexSSandQfun, it] + Qfun[0, it + 1]
     else:
         sPred = xPred[:, 4]
-        # print "Here 3", sum(sPred > TrackLength)
         predCurrLap = LMPC.N - sum(sPred > TrackLength)
         currLapTime = LMPC.LapTime
         Sel_Qfun = Qfun[indexSSandQfun, it] + currLapTime + predCurrLap
 
-    return SS_Points, Sel_Qfun
+    return SS_Points, SSu_Points, Sel_Qfun
 
 def _ComputeCost(x, u, TrackLength):
     Cost = 10000 * np.ones((x.shape[0]))  # The cost has the same elements of the vector x --> time +1
@@ -566,8 +575,8 @@ def _LMPC_BuildMatEqConst(LMPC, A, B, C, N, n, d):
 def _LMPC_GetPred(Solution,n,d,N, np):
     xPred = np.squeeze(np.transpose(np.reshape((Solution[np.arange(n*(N+1))]),(N+1,n))))
     uPred = np.squeeze(np.transpose(np.reshape((Solution[n*(N+1)+np.arange(d*N)]),(N, d))))
-    lambd = Solution[n*(N+1)+d*N:Solution.shape[0]-n]
-    slack = Solution[Solution.shape[0]-n-2*N:]
+    lambd = Solution[(n*(N+1)+d*N):(Solution.shape[0]-n-2*N)]
+    slack = Solution[Solution.shape[0]-n-2*N:Solution.shape[0]-2*N]
     laneSlack = Solution[Solution.shape[0]-2*N:]
 
     # print np.sum(np.abs(laneSlack))
@@ -587,13 +596,13 @@ def _LMPC_EstimateABC(ControllerLMPC, sortedLapTime):
     N               = ControllerLMPC.N
     n               = ControllerLMPC.n
     d               = ControllerLMPC.d
-    SS              = ControllerLMPC.SS
-    uSS             = ControllerLMPC.uSS
     TimeSS          = ControllerLMPC.TimeSS
+    LapCounter      = ControllerLMPC.LapCounter
     PointAndTangent = ControllerLMPC.map.PointAndTangent
     dt              = ControllerLMPC.dt
     it              = ControllerLMPC.it
-    p               = ControllerLMPC.p
+    SS              = ControllerLMPC.SS
+    uSS             = ControllerLMPC.uSS
 
     ParallelComputation = 0
     Atv = []; Btv = []; Ctv = []; indexUsed_list = []
@@ -601,33 +610,18 @@ def _LMPC_EstimateABC(ControllerLMPC, sortedLapTime):
     usedIt = sortedLapTime[0:ControllerLMPC.itUsedSysID] # range(ControllerLMPC.it-ControllerLMPC.itUsedSysID, ControllerLMPC.it)
     MaxNumPoint = 40  # Need to reason on how these points are selected
 
-    if ParallelComputation == 1:
-        # Parallel Implementation
-        Fun = partial(RegressionAndLinearization,LinPoints, LinInput, usedIt, SS, uSS, TimeSS,
-                       MaxNumPoint, qp, n, d, matrix, PointAndTangent, dt)
-
-        index = np.arange(0, N)  # Create the index vector
-
-        Res = p.map(Fun, index)  # Run the process in parallel
-        ParallelResutl = np.asarray(Res)
 
     for i in range(0, N):
-        if ParallelComputation == 0:
-           Ai, Bi, Ci, indexSelected = RegressionAndLinearization(LinPoints, LinInput, usedIt, SS, uSS, TimeSS,
-                                                               MaxNumPoint, qp, n, d, matrix, PointAndTangent, dt, i)
-           Atv.append(Ai)
-           Btv.append(Bi)
-           Ctv.append(Ci)
-           indexUsed_list.append(indexSelected)
-        else:
-           Atv.append(ParallelResutl[i][0])
-           Btv.append(ParallelResutl[i][1])
-           Ctv.append(ParallelResutl[i][2])
-           indexUsed_list.append(ParallelResutl[i][3])
+       Ai, Bi, Ci, indexSelected = RegressionAndLinearization(LinPoints, LinInput, usedIt, SS, uSS, TimeSS,
+                                                           MaxNumPoint, qp, n, d, matrix, PointAndTangent, dt, i)
+       Atv.append(Ai)
+       Btv.append(Bi)
+       Ctv.append(Ci)
+       indexUsed_list.append(indexSelected)
 
     return Atv, Btv, Ctv, indexUsed_list
 
-def RegressionAndLinearization(LinPoints, LinInput, usedIt, SS, uSS, TimeSS, MaxNumPoint, qp, n, d, matrix, PointAndTangent, dt, i):
+def RegressionAndLinearization(LinPoints, LinInput, usedIt, SS, uSS, LapCounter, MaxNumPoint, qp, n, d, matrix, PointAndTangent, dt, i):
 
 
     x0 = LinPoints[i, :]
@@ -657,8 +651,8 @@ def RegressionAndLinearization(LinPoints, LinInput, usedIt, SS, uSS, TimeSS, Max
 
     indexSelected = []
     K = []
-    for i in usedIt:
-        indexSelected_i, K_i = ComputeIndex(h, SS, uSS, TimeSS, i, xLin, stateFeatures, scaling, MaxNumPoint,
+    for ii in usedIt:
+        indexSelected_i, K_i = ComputeIndex(h, SS, uSS, LapCounter, ii, xLin, stateFeatures, scaling, MaxNumPoint,
                                             ConsiderInput)
         indexSelected.append(indexSelected_i)
         K.append(K_i)
@@ -796,7 +790,7 @@ def LMPC_LocLinReg(Q, b, stateFeatures, inputFeatures, qp):
 
     return A, B, C
 
-def ComputeIndex(h, SS, uSS, TimeSS, it, x0, stateFeatures, scaling, MaxNumPoint, ConsiderInput):
+def ComputeIndex(h, SS, uSS, LapCounter, it, x0, stateFeatures, scaling, MaxNumPoint, ConsiderInput):
     import numpy as np
     from numpy import linalg as la
     import datetime
@@ -806,14 +800,14 @@ def ComputeIndex(h, SS, uSS, TimeSS, it, x0, stateFeatures, scaling, MaxNumPoint
     startTimer = datetime.datetime.now()  # Start timer for LMPC iteration
 
     # What to learn a model such that: x_{k+1} = A x_k  + B u_k + C
-    oneVec = np.ones( (SS[0:TimeSS[it], :, it].shape[0]-1, 1) )
+    oneVec = np.ones( (SS[0:LapCounter[it], :, it].shape[0]-1, 1) )
 
     x0Vec = (np.dot( np.array([x0]).T, oneVec.T )).T
 
     if ConsiderInput == 1:
-        DataMatrix = np.hstack((SS[0:TimeSS[it]-1, stateFeatures, it], uSS[0:TimeSS[it]-1, :, it]))
+        DataMatrix = np.hstack((SS[0:LapCounter[it]-1, stateFeatures, it], uSS[0:LapCounter[it]-1, :, it]))
     else:
-        DataMatrix = SS[0:TimeSS[it]-1, stateFeatures, it]
+        DataMatrix = SS[0:LapCounter[it]-1, stateFeatures, it]
 
     # np.savetxt('A.csv', SS[0:TimeSS[it]-1, stateFeatures, it], delimiter=',', fmt='%f')
     # np.savetxt('B.csv', SS[0:TimeSS[it], :, it][0:-1, stateFeatures], delimiter=',', fmt='%f')

src/fnc/Track.py
@@ -7,37 +7,37 @@ class Map():
     Attributes:
         getGlobalPosition: convert position from (s, ey) to (X,Y)
     """
-    def __init__(self, width):
+    def __init__(self, halfWidth):
         """Initialization
-        width: track width
+        halfWidth: track halfWidth
         Modify the vector spec to change the geometry of the track
         """
         # Goggle-shaped track
-        self.slack = 0.15
-        self.width = width
-        spec = np.array([[60 * 0.03, 0],
-                         [80 * 0.03, -80 * 0.03 * 2 / np.pi],
-                         # Note s = 1 * np.pi / 2 and r = -1 ---> Angle spanned = np.pi / 2
-                         [20 * 0.03, 0],
-                         [80 * 0.03, -80 * 0.03 * 2 / np.pi],
-                         [40 * 0.03, +40 * 0.03 * 10 / np.pi],
-                         [60 * 0.03, -60 * 0.03 * 5 / np.pi],
-                         [40 * 0.03, +40 * 0.03 * 10 / np.pi],
-                         [80 * 0.03, -80 * 0.03 * 2 / np.pi],
-                         [20 * 0.03, 0],
-                         [80 * 0.03, -80 * 0.03 * 2 / np.pi]])
-
-        # # L-shaped track
-        # self.width = 0.4
-        # self.slack = 0.45
-        # lengthCurve = 4.5
-        # spec = np.array([[1.0, 0],
-        #                  [lengthCurve, lengthCurve / np.pi],
+        # self.slack = 0.15
+        # self.halfWidth = halfWidth
+        # spec = np.array([[60 * 0.03, 0],
+        #                  [80 * 0.03, -80 * 0.03 * 2 / np.pi],
         #                  # Note s = 1 * np.pi / 2 and r = -1 ---> Angle spanned = np.pi / 2
-        #                  [lengthCurve / 2, -lengthCurve / np.pi],
-        #                  [lengthCurve, lengthCurve / np.pi],
-        #                  [lengthCurve / np.pi * 2, 0],
-        #                  [lengthCurve / 2, lengthCurve / np.pi]])
+        #                  [20 * 0.03, 0],
+        #                  [80 * 0.03, -80 * 0.03 * 2 / np.pi],
+        #                  [40 * 0.03, +40 * 0.03 * 10 / np.pi],
+        #                  [60 * 0.03, -60 * 0.03 * 5 / np.pi],
+        #                  [40 * 0.03, +40 * 0.03 * 10 / np.pi],
+        #                  [80 * 0.03, -80 * 0.03 * 2 / np.pi],
+        #                  [20 * 0.03, 0],
+        #                  [80 * 0.03, -80 * 0.03 * 2 / np.pi]])
+
+        # L-shaped track
+        self.halfWidth = 0.4
+        self.slack = 0.45
+        lengthCurve = 4.5
+        spec = np.array([[1.0, 0],
+                         [lengthCurve, lengthCurve / np.pi],
+                         # Note s = 1 * np.pi / 2 and r = -1 ---> Angle spanned = np.pi / 2
+                         [lengthCurve / 2, -lengthCurve / np.pi],
+                         [lengthCurve, lengthCurve / np.pi],
+                         [lengthCurve / np.pi * 2, 0],
+                         [lengthCurve / 2, lengthCurve / np.pi]])
 
 
         # spec = np.array([[1.0, 0],
@@ -228,7 +228,7 @@ class Map():
                         s       = s_local + PointAndTangent[i, 3]
                         ey      = la.norm(v1) * np.sin(angle)
 
-                        if np.abs(ey)<= self.width + self.slack:
+                        if np.abs(ey)<= self.halfWidth + self.slack:
                             CompletedFlag = 1
 
             else:
@@ -273,7 +273,7 @@ class Map():
                         psi_unwrap = np.unwrap([ang + arc2, psi])[1]
                         epsi = psi_unwrap - (ang + arc2)
 
-                        if np.abs(ey) <= self.width + self.slack:
+                        if np.abs(ey) <= self.halfWidth + self.slack:
                             CompletedFlag = 1
 
         if epsi>1.0:

src/fnc/plot.py
@@ -11,8 +11,8 @@ def plotTrajectory(map, x, x_glob, u):
     Points2 = np.zeros((Points, 2))
     Points0 = np.zeros((Points, 2))
     for i in range(0, int(Points)):
-        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.width)
-        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.width)
+        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.halfWidth)
+        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.halfWidth)
         Points0[i, :] = map.getGlobalPosition(i * 0.1, 0)
 
     plt.figure()
@@ -47,7 +47,7 @@ def plotTrajectory(map, x, x_glob, u):
 
 def plotClosedLoopLMPC(LMPController, map):
     SS_glob = LMPController.SS_glob
-    TimeSS  = LMPController.TimeSS
+    LapCounter  = LMPController.LapCounter
     SS      = LMPController.SS
     uSS     = LMPController.uSS
 
@@ -57,8 +57,8 @@ def plotClosedLoopLMPC(LMPController, map):
     Points2 = np.zeros((Points, 2))
     Points0 = np.zeros((Points, 2))
     for i in range(0, int(Points)):
-        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.width)
-        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.width)
+        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.halfWidth)
+        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.halfWidth)
         Points0[i, :] = map.getGlobalPosition(i * 0.1, 0)
 
     plt.figure(1)
@@ -68,42 +68,42 @@ def plotClosedLoopLMPC(LMPController, map):
     plt.plot(Points2[:, 0], Points2[:, 1], '-b')
 
     for i in range(2, TotNumberIt):
-        plt.plot(SS_glob[0:TimeSS[i], 4, i], SS_glob[0:TimeSS[i], 5, i], '-r')
+        plt.plot(SS_glob[0:LapCounter[i], 4, i], SS_glob[0:LapCounter[i], 5, i], '-r')
 
     plt.figure(2)
     plt.subplot(711)
     for i in range(2, TotNumberIt):
-        plt.plot(SS[0:TimeSS[i], 4, i], SS[0:TimeSS[i], 0, i], '-o')
+        plt.plot(SS[0:LapCounter[i], 4, i], SS[0:LapCounter[i], 0, i], '-o')
     plt.ylabel('vx')
     plt.subplot(712)
     for i in range(2, TotNumberIt):
-        plt.plot(SS[0:TimeSS[i], 4, i], SS[0:TimeSS[i], 1, i], '-o')
+        plt.plot(SS[0:LapCounter[i], 4, i], SS[0:LapCounter[i], 1, i], '-o')
     plt.ylabel('vy')
     plt.subplot(713)
     for i in range(2, TotNumberIt):
-        plt.plot(SS[0:TimeSS[i], 4, i], SS[0:TimeSS[i], 2, i], '-o')
+        plt.plot(SS[0:LapCounter[i], 4, i], SS[0:LapCounter[i], 2, i], '-o')
     plt.ylabel('wz')
     plt.subplot(714)
     for i in range(2, TotNumberIt):
-        plt.plot(SS[0:TimeSS[i], 4, i], SS[0:TimeSS[i], 3, i], '-o')
+        plt.plot(SS[0:LapCounter[i], 4, i], SS[0:LapCounter[i], 3, i], '-o')
     plt.ylabel('epsi')
     plt.subplot(715)
     for i in range(2, TotNumberIt):
-        plt.plot(SS[0:TimeSS[i], 4, i], SS[0:TimeSS[i], 5, i], '-o')
+        plt.plot(SS[0:LapCounter[i], 4, i], SS[0:LapCounter[i], 5, i], '-o')
     plt.ylabel('ey')
     plt.subplot(716)
     for i in range(2, TotNumberIt):
-        plt.plot(uSS[0:TimeSS[i] - 1, 0, i], '-o')
+        plt.plot(uSS[0:LapCounter[i] - 1, 0, i], '-o')
     plt.ylabel('Steering')
     plt.subplot(717)
     for i in range(2, TotNumberIt):
-        plt.plot(uSS[0:TimeSS[i] - 1, 1, i], '-o')
+        plt.plot(uSS[0:LapCounter[i] - 1, 1, i], '-o')
     plt.ylabel('Acc')
 
 
 def animation_xy(map, LMPCOpenLoopData, LMPController, it):
     SS_glob = LMPController.SS_glob
-    TimeSS = LMPController.TimeSS
+    LapCounter = LMPController.LapCounter
     SS = LMPController.SS
     uSS = LMPController.uSS
 
@@ -111,9 +111,10 @@ def animation_xy(map, LMPCOpenLoopData, LMPController, it):
     Points1 = np.zeros((Points, 2))
     Points2 = np.zeros((Points, 2))
     Points0 = np.zeros((Points, 2))
+
     for i in range(0, int(Points)):
-        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.width)
-        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.width)
+        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.halfWidth)
+        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.halfWidth)
         Points0[i, :] = map.getGlobalPosition(i * 0.1, 0)
 
     plt.figure(200)
@@ -121,7 +122,7 @@ def animation_xy(map, LMPCOpenLoopData, LMPController, it):
     plt.plot(Points0[:, 0], Points0[:, 1], '--')
     plt.plot(Points1[:, 0], Points1[:, 1], '-b')
     plt.plot(Points2[:, 0], Points2[:, 1], '-b')
-    plt.plot(SS_glob[0:TimeSS[it], 4, it], SS_glob[0:TimeSS[it], 5, it], '-ok', label="Closed-loop trajectory",zorder=-1)
+    plt.plot(SS_glob[0:LapCounter[it], 4, it], SS_glob[0:LapCounter[it], 5, it], '-ok', label="Closed-loop trajectory",zorder=-1)
 
     ax = plt.axes()
     SSpoints_x = []; SSpoints_y = []
@@ -141,7 +142,7 @@ def animation_xy(map, LMPCOpenLoopData, LMPController, it):
 
     N = LMPController.N
     numSS_Points = LMPController.numSS_Points
-    for i in range(0, int(LMPController.TimeSS[it])):
+    for i in range(0, int(LMPController.LapCounter[it])):
 
         xPred = np.zeros((N+1, 1)); yPred = np.zeros((N+1, 1))
         SSpoints_x = np.zeros((numSS_Points, 1)); SSpoints_y = np.zeros((numSS_Points, 1))
@@ -177,7 +178,7 @@ def animation_xy(map, LMPCOpenLoopData, LMPController, it):
 
 def animation_states(map, LMPCOpenLoopData, LMPController, it):
     SS_glob = LMPController.SS_glob
-    TimeSS = LMPController.TimeSS
+    LapCounter = LMPController.LapCounter
     SS = LMPController.SS
     uSS = LMPController.uSS
 
@@ -185,7 +186,7 @@ def animation_states(map, LMPCOpenLoopData, LMPController, it):
     fig = plt.figure(100)
 
     axvx = fig.add_subplot(3, 2, 1)
-    plt.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 0, it], '-ok', label="Closed-loop trajectory")
+    plt.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 0, it], '-ok', label="Closed-loop trajectory")
     lineSSvx, = axvx.plot(xdata, ydata, 'sb-', label="SS")
     linevx, = axvx.plot(xdata, ydata, 'or-', label="Predicted Trajectory")
     plt.ylabel("vx")
@@ -195,28 +196,28 @@ def animation_states(map, LMPCOpenLoopData, LMPController, it):
                 mode="expand", borderaxespad=0, ncol=3)
 
     axvy = fig.add_subplot(3, 2, 2)
-    axvy.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 1, it], '-ok')
+    axvy.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 1, it], '-ok')
     lineSSvy, = axvy.plot(xdata, ydata, 'sb-')
     linevy, = axvy.plot(xdata, ydata, 'or-')
     plt.ylabel("vy")
     plt.xlabel("s")
 
     axwz = fig.add_subplot(3, 2, 3)
-    axwz.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 2, it], '-ok')
+    axwz.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 2, it], '-ok')
     lineSSwz, = axwz.plot(xdata, ydata, 'sb-')
     linewz, = axwz.plot(xdata, ydata, 'or-')
     plt.ylabel("wz")
     plt.xlabel("s")
 
     axepsi = fig.add_subplot(3, 2, 4)
-    axepsi.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 3, it], '-ok')
+    axepsi.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 3, it], '-ok')
     lineSSepsi, = axepsi.plot(xdata, ydata, 'sb-')
     lineepsi, = axepsi.plot(xdata, ydata, 'or-')
     plt.ylabel("epsi")
     plt.xlabel("s")
 
     axey = fig.add_subplot(3, 2, 5)
-    axey.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 5, it], '-ok')
+    axey.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 5, it], '-ok')
     lineSSey, = axey.plot(xdata, ydata, 'sb-')
     lineey, = axey.plot(xdata, ydata, 'or-')
     plt.ylabel("ey")
@@ -227,8 +228,8 @@ def animation_states(map, LMPCOpenLoopData, LMPController, it):
     Points2 = np.zeros((Points, 2))
     Points0 = np.zeros((Points, 2))
     for i in range(0, int(Points)):
-        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.width)
-        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.width)
+        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.halfWidth)
+        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.halfWidth)
         Points0[i, :] = map.getGlobalPosition(i * 0.1, 0)
 
     axtr = fig.add_subplot(3, 2, 6)
@@ -236,7 +237,7 @@ def animation_states(map, LMPCOpenLoopData, LMPController, it):
     plt.plot(Points0[:, 0], Points0[:, 1], '--')
     plt.plot(Points1[:, 0], Points1[:, 1], '-b')
     plt.plot(Points2[:, 0], Points2[:, 1], '-b')
-    plt.plot(SS_glob[0:TimeSS[it], 4, it], SS_glob[0:TimeSS[it], 5, it], '-ok')
+    plt.plot(SS_glob[0:LapCounter[it], 4, it], SS_glob[0:LapCounter[it], 5, it], '-ok')
 
     SSpoints_x = []; SSpoints_y = []
     xPred = []; yPred = []
@@ -245,7 +246,7 @@ def animation_states(map, LMPCOpenLoopData, LMPController, it):
 
     N = LMPController.N
     numSS_Points = LMPController.numSS_Points
-    for i in range(0, int(LMPController.TimeSS[it])):
+    for i in range(0, int(LMPController.LapCounter[it])):
 
         xPred    = LMPCOpenLoopData.PredictedStates[:, :, i, it]
         SSpoints = LMPCOpenLoopData.SSused[:, :, i, it]
@@ -291,7 +292,7 @@ def animation_states(map, LMPCOpenLoopData, LMPController, it):
 
 def saveGif_xyResults(map, LMPCOpenLoopData, LMPController, it):
     SS_glob = LMPController.SS_glob
-    TimeSS = LMPController.TimeSS
+    LapCounter = LMPController.LapCounter
     SS = LMPController.SS
     uSS = LMPController.uSS
 
@@ -300,8 +301,8 @@ def saveGif_xyResults(map, LMPCOpenLoopData, LMPController, it):
     Points2 = np.zeros((Points, 2))
     Points0 = np.zeros((Points, 2))
     for i in range(0, int(Points)):
-        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.width)
-        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.width)
+        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.halfWidth)
+        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.halfWidth)
         Points0[i, :] = map.getGlobalPosition(i * 0.1, 0)
 
     fig = plt.figure(101)
@@ -311,7 +312,7 @@ def saveGif_xyResults(map, LMPCOpenLoopData, LMPController, it):
     plt.plot(Points0[:, 0], Points0[:, 1], '--')
     plt.plot(Points1[:, 0], Points1[:, 1], '-b')
     plt.plot(Points2[:, 0], Points2[:, 1], '-b')
-    plt.plot(SS_glob[0:TimeSS[it], 4, it], SS_glob[0:TimeSS[it], 5, it], '-ok', label="Closed-loop trajectory", markersize=1,zorder=-1)
+    plt.plot(SS_glob[0:LapCounter[it], 4, it], SS_glob[0:LapCounter[it], 5, it], '-ok', label="Closed-loop trajectory", markersize=1,zorder=-1)
 
     ax = plt.axes()
     SSpoints_x = []; SSpoints_y = []
@@ -360,7 +361,7 @@ def saveGif_xyResults(map, LMPCOpenLoopData, LMPController, it):
 
         rec.set_xy(np.array([car_x, car_y]).T)
 
-    anim = FuncAnimation(fig, update, frames=np.arange(0, int(LMPController.TimeSS[it])), interval=100)
+    anim = FuncAnimation(fig, update, frames=np.arange(0, int(LMPController.LapCounter[it])), interval=100)
 
     anim.save('ClosedLoop.gif', dpi=80, writer='imagemagick')
 
@@ -368,7 +369,7 @@ def saveGif_xyResults(map, LMPCOpenLoopData, LMPController, it):
 
 def Save_statesAnimation(map, LMPCOpenLoopData, LMPController, it):
     SS_glob = LMPController.SS_glob
-    TimeSS = LMPController.TimeSS
+    LapCounter = LMPController.LapCounter
     SS = LMPController.SS
     uSS = LMPController.uSS
 
@@ -377,7 +378,7 @@ def Save_statesAnimation(map, LMPCOpenLoopData, LMPController, it):
     fig.set_tight_layout(True)
 
     axvx = fig.add_subplot(3, 2, 1)
-    plt.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 0, it], '-ok', label="Closed-loop trajectory")
+    plt.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 0, it], '-ok', label="Closed-loop trajectory")
     lineSSvx, = axvx.plot(xdata, ydata, 'sb-', label="SS")
     linevx, = axvx.plot(xdata, ydata, 'or-', label="Predicted Trajectory")
     plt.ylabel("vx")
@@ -387,28 +388,28 @@ def Save_statesAnimation(map, LMPCOpenLoopData, LMPController, it):
                 mode="expand", borderaxespad=0, ncol=3)
 
     axvy = fig.add_subplot(3, 2, 2)
-    axvy.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 1, it], '-ok')
+    axvy.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 1, it], '-ok')
     lineSSvy, = axvy.plot(xdata, ydata, 'sb-')
     linevy, = axvy.plot(xdata, ydata, 'or-')
     plt.ylabel("vy")
     plt.xlabel("s")
 
     axwz = fig.add_subplot(3, 2, 3)
-    axwz.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 2, it], '-ok')
+    axwz.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 2, it], '-ok')
     lineSSwz, = axwz.plot(xdata, ydata, 'sb-')
     linewz, = axwz.plot(xdata, ydata, 'or-')
     plt.ylabel("wz")
     plt.xlabel("s")
 
     axepsi = fig.add_subplot(3, 2, 4)
-    axepsi.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 3, it], '-ok')
+    axepsi.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 3, it], '-ok')
     lineSSepsi, = axepsi.plot(xdata, ydata, 'sb-')
     lineepsi, = axepsi.plot(xdata, ydata, 'or-')
     plt.ylabel("epsi")
     plt.xlabel("s")
 
     axey = fig.add_subplot(3, 2, 5)
-    axey.plot(SS[0:TimeSS[it], 4, it], SS[0:TimeSS[it], 5, it], '-ok')
+    axey.plot(SS[0:LapCounter[it], 4, it], SS[0:LapCounter[it], 5, it], '-ok')
     lineSSey, = axey.plot(xdata, ydata, 'sb-')
     lineey, = axey.plot(xdata, ydata, 'or-')
     plt.ylabel("ey")
@@ -419,8 +420,8 @@ def Save_statesAnimation(map, LMPCOpenLoopData, LMPController, it):
     Points2 = np.zeros((Points, 2))
     Points0 = np.zeros((Points, 2))
     for i in range(0, int(Points)):
-        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.width)
-        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.width)
+        Points1[i, :] = map.getGlobalPosition(i * 0.1, map.halfWidth)
+        Points2[i, :] = map.getGlobalPosition(i * 0.1, -map.halfWidth)
         Points0[i, :] = map.getGlobalPosition(i * 0.1, 0)
 
     axtr = fig.add_subplot(3, 2, 6)
@@ -428,7 +429,7 @@ def Save_statesAnimation(map, LMPCOpenLoopData, LMPController, it):
     plt.plot(Points0[:, 0], Points0[:, 1], '--')
     plt.plot(Points1[:, 0], Points1[:, 1], '-b')
     plt.plot(Points2[:, 0], Points2[:, 1], '-b')
-    plt.plot(SS_glob[0:TimeSS[it], 4, it], SS_glob[0:TimeSS[it], 5, it], '-ok')
+    plt.plot(SS_glob[0:LapCounter[it], 4, it], SS_glob[0:LapCounter[it], 5, it], '-ok')
 
     SSpoints_x = []; SSpoints_y = []
     xPred = []; yPred = []
@@ -468,6 +469,6 @@ def Save_statesAnimation(map, LMPCOpenLoopData, LMPController, it):
         line_tr.set_data(xPred, yPred)
         SSpoints_tr.set_data(SSpoints_x, SSpoints_y)
 
-    anim = FuncAnimation(fig, update, frames=np.arange(0, int(LMPController.TimeSS[it])), interval=100)
+    anim = FuncAnimation(fig, update, frames=np.arange(0, int(LMPController.LapCounter[it])), interval=100)
 
     anim.save('ClosedLoopStates.gif', dpi=80, writer='imagemagick')

src/main.py
@@ -42,10 +42,10 @@ import pickle
 # ======================================================================================================================
 # ============================ Choose which controller to run ==========================================================
 # ======================================================================================================================
-RunPID     = 1; plotFlag       = 0
-RunMPC     = 1; plotFlagMPC    = 0
-RunMPC_tv  = 1; plotFlagMPC_tv = 0
-RunLMPC    = 1; plotFlagLMPC   = 0; animation_xyFlag = 1; animation_stateFlag = 0
+RunPID     = 0; plotFlag       = 0
+RunMPC     = 0; plotFlagMPC    = 0
+RunMPC_tv  = 0; plotFlagMPC_tv = 0
+RunLMPC    = 0; plotFlagLMPC   = 0; animation_xyFlag = 1; animation_stateFlag = 0
 
 # ======================================================================================================================
 # ============================ Initialize parameters for path following ================================================
@@ -70,20 +70,23 @@ simulator = Simulator(map)                # Initialize the Simulator
 # ==================================== Initialize parameters for LMPC ==================================================
 # ======================================================================================================================
 TimeLMPC   = 400              # Simulation time
-Laps       = 10+2             # Total LMPC laps
+Laps       = 30+2             # Total LMPC laps
 
 # Safe Set Parameters
 LMPC_Solver = "CVX"           # Can pick CVX for cvxopt or OSQP. For OSQP uncomment line 14 in LMPC.py
 numSS_it = 2                  # Number of trajectories used at each iteration to build the safe set
-numSS_Points = 32 + N         # Number of points to select from each trajectory to build the safe set
-shift = 0                     # Given the closed point, x_t^j, to the x(t) select the SS points from x_{t+shift}^j
+numSS_Points = 40         # Number of points to select from each trajectory to build the safe set
 
 # Tuning Parameters
 Qslack  =  5 * np.diag([10, 1, 1, 1, 10, 1])            # Cost on the slack variable for the terminal constraint
-Qlane   =  1 * np.array([0, 10])                      # Quadratic and linear slack lane cost
+Qlane   =  1 * np.array([0, 10])                        # Quadratic and linear slack lane cost
 Q_LMPC  =  0 * np.diag([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])  # State cost x = [vx, vy, wz, epsi, s, ey]
 R_LMPC  =  0 * np.diag([1.0, 1.0])                      # Input cost u = [delta, a]
-dR_LMPC = 10 * np.array([1.0, 10.0])                     # Input rate cost u
+dR_LMPC = 10 * np.array([1.0, 10.0])                    # Input rate cost u
+
+inputConstr = np.array([[0.5, 0.5],                     # Min Steering and Max Steering
+                        [1.0, 1.0]])                    # Min Acceleration and Max Acceleration
+
 
 # Initialize LMPC simulator
 LMPCSimulator = Simulator(map, 1, 1)
@@ -152,7 +155,7 @@ ClosedLoopLMPC = ClosedLoopData(dt, TimeLMPC, v0)
 LMPCOpenLoopData = LMPCprediction(N, n, d, TimeLMPC, numSS_Points, Laps)
 LMPCSimulator = Simulator(map, 1, 1)
 
-LMPController = ControllerLMPC(numSS_Points, numSS_it, N, Qslack, Qlane, Q_LMPC, R_LMPC, dR_LMPC, n, d, shift, dt, map, Laps, TimeLMPC, LMPC_Solver)
+LMPController = ControllerLMPC(numSS_Points, numSS_it, N, Qslack, Qlane, Q_LMPC, R_LMPC, dR_LMPC, dt, map, Laps, TimeLMPC, LMPC_Solver, inputConstr)
 LMPController.addTrajectory(ClosedLoopDataPID)
 LMPController.addTrajectory(ClosedLoopDataLTV_MPC)
 
@@ -209,15 +212,15 @@ if plotFlagLMPC == 1:
     plotClosedLoopLMPC(LMPController, map)
 
 if animation_xyFlag == 1:
-    animation_xy(map, LMPCOpenLoopData, LMPController, 11)
+    animation_xy(map, LMPCOpenLoopData, LMPController, 30)
 
 if animation_stateFlag == 1:
     animation_states(map, LMPCOpenLoopData, LMPController, 11)
 
-unityTestChangeOfCoordinates(map, ClosedLoopDataPID)
-unityTestChangeOfCoordinates(map, ClosedLoopDataLTI_MPC)
-unityTestChangeOfCoordinates(map, ClosedLoopLMPC)
+# unityTestChangeOfCoordinates(map, ClosedLoopDataPID)
+# unityTestChangeOfCoordinates(map, ClosedLoopDataLTI_MPC)
+# unityTestChangeOfCoordinates(map, ClosedLoopLMPC)
 
-# saveGif_xyResults(map, LMPCOpenLoopData, LMPController, 36)
+saveGif_xyResults(map, LMPCOpenLoopData, LMPController, 30)
 # Save_statesAnimation(map, LMPCOpenLoopData, LMPController, 5)
 plt.show()