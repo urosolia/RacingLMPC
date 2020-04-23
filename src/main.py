dd91b7fc459187214e16033bccb1828f6b88b264
urosolia <ugo.rosolia@berkeley.edu>
2020-04-22 23:45:56 -0700

    x

--- src/main.py
+++ src/main.py
@@ -39,10 +39,10 @@ import pickle
 # ======================================================================================================================
 # ============================ Choose which controller to run ==========================================================
 # ======================================================================================================================
-RunPID     = 0; plotFlag       = 0
-RunMPC     = 0; plotFlagMPC    = 0
-RunMPC_tv  = 0; plotFlagMPC_tv = 0
-RunLMPC    = 0; plotFlagLMPC   = 1; animation_xyFlag = 0; animation_stateFlag = 0
+RunPID     = 1; plotFlag       = 0
+RunMPC     = 1; plotFlagMPC    = 0
+RunMPC_tv  = 1; plotFlagMPC_tv = 0
+RunLMPC    = 1; plotFlagLMPC   = 1; animation_xyFlag = 1; animation_stateFlag = 0
 
 # ======================================================================================================================
 # ============================ Initialize parameters for path following ================================================
